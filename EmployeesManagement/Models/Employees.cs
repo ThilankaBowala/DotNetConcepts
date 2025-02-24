37b911d79d70475269dc710e0e6830dab87250a1
Thilanka Bowala <38552205+ThilankaCTS@users.noreply.github.com>
2025-02-24 12:47:39 +0530

    Install EF Core nugets

---
+++ EmployeesManagement/Models/Employees.cs
@@ -0,0 +1,17 @@
+﻿namespace EmployeesManagement.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string? MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public int PhoneNumber { get; set; }
+    }
+}
